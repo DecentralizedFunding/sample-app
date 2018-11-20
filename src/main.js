f291f3773c1197a04aa87da54adc602f80d44483
mktia <endoplasmicreticulum2468741059@gmail.com>
2018-11-20 12:23:40 +0900

    add firebase configuration

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+
+// require('dotenv').config()
 
 Vue.config.productionTip = false
+
+/* eslint-disable */
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCneMwRSl1NCBrJYeJgNkqgueH7e-28qQk",
+  authDomain: "decentralized-funding.firebaseapp.com",
+  databaseURL: "https://decentralized-funding.firebaseio.com",
+  projectId: "decentralized-funding",
+  storageBucket: "decentralized-funding.appspot.com",
+  messagingSenderId: "568854250007"
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
