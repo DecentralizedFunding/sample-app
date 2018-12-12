ef954127e961fdd9e9fccfb44501e184776f0f0f
mktia <endoplasmicreticulum2468741059@gmail.com>
2018-12-12 15:33:40 +0900

    add address validator in creating project

--- src/components/StartProject.vue
+++ src/components/StartProject.vue
@@ -3,21 +3,23 @@
     <h2>Create Project</h2>
     <b-form class="mx-auto" style="width: 320px;" @submit="onSubmit">
       <b-form-group label="アイキャッチ画像">
-        <b-form-file v-model="image" accept=".jpg, .png" placeholder="JPG または PNG" required></b-form-file>
+        <b-form-file v-model="form.image" accept=".jpg, .png" placeholder="JPG または PNG" required></b-form-file>
       </b-form-group>
       <b-form-group label="タイトル">
-        <b-form-input v-model="title" type="text" placeholder="タイトル (目的)" required></b-form-input>
+        <b-form-input v-model="form.title" type="text" placeholder="タイトル (目的)" required></b-form-input>
       </b-form-group>
       <b-form-group label="やりたいこと">
-        <b-form-input v-model="content" type="text" placeholder="説明文" required></b-form-input>
+        <b-form-input v-model="form.content" type="text" placeholder="説明文" required></b-form-input>
       </b-form-group>
       <b-form-group label="目標金額">
-        <b-form-input v-model="goal" type="text" placeholder="ETH" required></b-form-input>
+        <b-form-input v-model="form.goal" type="text" placeholder="ETH" required></b-form-input>
       </b-form-group>
       <b-form-group label="期限">
-        <b-form-input v-model="date" type="date" required></b-form-input>
+        <b-form-input v-model="form.date" type="date" required></b-form-input>
       </b-form-group>
-      <b-button @click="startProject" type="submit">つくる</b-button>
+      <b-button v-if="isFormFilled" @click="startProject" type="submit" variant="primary">つくる</b-button>
+      <b-button v-else variant="secondary" disabled>つくる</b-button>
+      <b-alert v-if="errorMessage" show variant="danger">{{ errorMessage }}</b-alert>
     </b-form>
     <router-link :to="{ name: 'TopPage' }">← トップに戻る</router-link>
   </div>
@@ -40,11 +42,25 @@ export default {
   name: 'CreateProject',
   data () {
     return {
-      image: null,
-      title: null,
-      content: null,
-      goal: null,
-      date: null,
+      account: null,
+      uid: null,
+      form: {
+        image: null,
+        title: null,
+        content: null,
+        goal: null,
+        date: null
+      },
+      errorMessage: null
+    }
+  },
+  computed: {
+    isFormFilled () {
+      if (this.form.image !== null && this.form.title !== null && this.form.content !== null && this.form.goal !== null && this.form.date !== null) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   beforeCreate () {
@@ -71,7 +87,7 @@ export default {
 
     web3.eth.getAccounts()
       .then((accounts) => this.account = accounts[0])
-      .catch(console.log)
+      .catch(console.error)
 
     web3.currentProvider.publicConfigStore.on('update', (info) => this.account = info.selectedAddress)
 
@@ -83,7 +99,7 @@ export default {
       .then((instance) => {
         var createdProject = instance.NewPJ()
         createdProject.watch((error, result) => {
-          if (!error && this.title !== null) {
+          if (!error && this.form.title !== null) {
             // Move to the project page after creating it
             var id = result.args.id.toNumber()
             this.$router.replace({ name: 'Project', params: { projectId: id }})
@@ -106,13 +122,16 @@ export default {
       })
     },
     startProject () {
-      var limit = new Date(this.date)
+      var limit = new Date(this.form.date)
       var contract
       var projectId
+      var uid
 
       firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
           this.$router.replace({ name: 'SignUp' })
+        } else {
+          uid = user.uid
         }
       })
 
@@ -121,7 +140,17 @@ export default {
       DFcore.deployed()
         .then((instance) => {
           contract = instance
-          return contract.makePJ(this.title, web3.utils.toWei(this.goal, 'ether'), limit.getTime())
+          return db.collection('users').doc(uid).get()
+        }).then((doc) => {
+          // get registered address from firestore
+          var registerdAddress = doc.data().address
+          // If using wallet is not registered, throw error
+          if (this.account !== registerdAddress) {
+            this.errorMessage = '登録したウォレットを使用してください'
+            throw new Error('Connected wallet address is not registered.')
+          }
+
+          return contract.makePJ(this.form.title, web3.utils.toWei(this.form.goal, 'ether'), limit.getTime())
         })
         .then(() => {
           return contract.getPJCount()
@@ -141,11 +170,11 @@ export default {
               throw new Error('This type of image is not adapted')
           }*/
           var newImageRef = storageRef.child(`images/projects/${projectId}`)
-          return newImageRef.put(this.image)
+          return newImageRef.put(this.form.image)
         })
         .then(() => {
           return db.collection('projects').doc(projectId.toString())
-            .set({description: this.content})
+            .set({description: this.form.content})
         })
         .catch((error) => console.error(error))
     }
