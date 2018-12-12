dbe817952b6bdbae6c064ec12bb7c9c3dca7c275
mktia <endoplasmicreticulum2468741059@gmail.com>
2018-12-12 16:44:43 +0900

    show projects user created

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="app">
-    <h2>User page</h2>
+    <h2>作成者ページ</h2>
     <b-link :href="`https://twitter.com/${twitter}`">@{{ twitter }}</b-link>
     <p>user: {{ userName }}</p>
+    <div class="project-box" v-for="project in projects" :key="project.id">
+      <router-link :to="{ name: 'Project', params: { projectId: project.id }}">
+        <b-card tag="article">
+          <h4>{{ project.title }}</h4>
+          <p>目標金額 {{ project.goal }} ETH</p>
+          <b-progress :value="project.funded" :max="project.goal" show-progress animated></b-progress>
+          <p v-if="project.left.days > 0">残り {{ project.left.days }} 日</p>
+          <p v-else-if="project.left.hours > 0">残り {{ project.left.hours }} 時間</p>
+          <p v-else-if="project.left.mitunes >= 0">残り {{ project.left.mitunes }} 分</p>
+          <p v-else-if="project.left.minutes < 0">終了</p>
+        </b-card>
+      </router-link>
+    </div>
+    <b-link :to="{ name: 'TopPage' }">トップへ戻る</b-link>
   </div>
 </template>
 
@@ -24,6 +38,7 @@ export default {
   name: 'User',
   data () {
     return {
+      projects: [],
       twitter: null,
       userName: null
     }
@@ -65,6 +80,47 @@ export default {
       })
       .catch(console.error)
   },
+  beforeMount () {
+    var contract
+    var projectLength
+    DFcore.deployed()
+      .then((instance) => {
+        contract = instance
+        return contract.getPJByOwner(this.account)
+      })
+      .then((list) => {
+        var promises = []
+        list.forEach((id) => {
+          promises.push(contract.getPJInfo(id.toNumber()))
+        })
+        projectLength = promises.length
+        return Promise.all(promises)
+      })
+      .then((projects) => {
+        for (var i = 0; i < projectLength; i++) {
+          // 1e21 対策
+          var goal = web3.utils.fromWei(web3.utils.toBN(projects[i][2]), 'ether')
+          var funded = web3.utils.fromWei(web3.utils.toBN(projects[i][3]), 'ether')
+          var unixTime = projects[i][4].toNumber()
+          var date = new Date(unixTime)
+          var left = unixTime - Date.now()
+
+          this.projects.unshift({
+            'id': projects[i][0].toNumber(),
+            'title': projects[i][1],
+            'goal': Number(goal),
+            'funded': Number(funded),
+            'limitTime': date.toLocaleDateString('ja-JP'),
+            'supporters': projects[i][5],
+            'left': {
+              'days': Math.floor(left / (24 * 60 * 60 * 1000)),
+              'hours': Math.floor(left / (60 * 60 * 1000)),
+              'minutes': Math.floor(left / (60 * 1000))
+            }
+          })
+        }
+      })
+  },
   methods: {
   }
 }
@@ -72,5 +128,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+article, .project-box {
+  width: 320px;
+}
 
+article:hover {
+  box-shadow: 0 0 2px 0 #007bff;
+}
+
+.project-box {
+  border: none;
+}
+
+.project-box a, .project-box *:hover {
+  color: inherit;
+  text-decoration: none;
+}
+
+.project-box #title {
+  font-weight: bold;
+}
 </style>
