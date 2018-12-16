b961257e856ffce10ef43f3b87cbefd6f87c678a
mktia <endoplasmicreticulum2468741059@gmail.com>
2018-12-16 11:46:31 +0900

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app" class="ml-3 mr-3">
+  <div id="app" class="container">
+    <div class="top-bar container-fluid fixed-top py-2">
+      <div class="top-bar_contents container d-flex align-items-center">
+        <b-link :to="{ name: 'Menu' }" class="menu-button mr-2">
+          <i class="fas fa-bars fa-lg"></i>
+        </b-link>
+        <b-link :to="{ name: 'TopPage' }" class="flex-grow-1">
+          <h1 class="text-dark">DF</h1>
+        </b-link>
+        <b-link v-if="!isLoggedIn" :to="{ name: 'Login' }">Login</b-link>
+        <b-link v-else :to="{ name: 'MyPage' }">MyPage</b-link>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -8,17 +20,63 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import firebase from 'firebase'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      isLoggedIn: false
+    }
+  },
+  beforeCreate () {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.isLoggedIn = true
+      }
+    })
+  }
 }
 </script>
 
 <style>
+.app {
+  padding-top: 64px;
+}
+
+.menu-button, .menu-button:hover {
+  color: #bbb;
+  margin-top: 2px;
+}
+
+.top-bar {
+  background-color: #fff;
+  box-shadow: 0 5px 5px -4px #bbb;
+}
+
+.top-bar a:hover {
+  text-decoration: none;
+}
+
+.top-bar h1 {
+  /*
+  background: linear-gradient(to top, #30cfd0, #330867);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;*/
+  font-size: 2rem;
+  margin-bottom: 0;
+}
+
+.top-bar_contents {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f9f9f9;
 }
 </style>
